749a3410a51c5b576c22cc0da9a135debba46b95
xiaohuoni <448627663@qq.com>
2021-03-12 11:00:54 +0800

    feat: support use Jsdelivr

--- README.md
+++ README.md
@@ -11,7 +11,11 @@ Configure in `.umirc.js`,
 
 ```js
 export default {
-  ghPages:{}
+  ghPages:{
+    dir:'dist',
+    useJsdelivr: false,
+    ...gh-pages#PublishOptions
+  }
 }
 ```
 
@@ -19,14 +23,67 @@ export default {
 
 exclude `dir` (default umi config `outputPath`), other options please checkout https://github.com/tschaub/gh-pages#options
 
-## Deploy
+### useJsdelivr
+
+A free CDN for Open Source - https://www.jsdelivr.com/
+
+### Deploy in GitHub pages
+
+config/config.ts
+
+```ts
+{
+  ghPages: {
+    useJsdelivr: true,
+  },
+}
 ```
-umi build && umi gh-pages
+
+```bash
+umi build
 ```
+
+or
+
+```bash
+$GH_PAGES_USE_JSDELIVR=true umi build
+```
+
+### Deploy in [vercel](https://vercel.com/) （now）
+
+config/config.ts
+
+```ts
+{
+  ghPages: {
+    branch: 'gh-pages',
+    silent: true,
+    repo: `https://${process.env.GH_TOKEN}@github.com/alitajs/alita-docs.git`,
+  },
+}
+```
+
+set the [environment variables](https://vercel.com/docs/environment-variables) in vercel, with Production.
+
+![image](https://user-images.githubusercontent.com/11746742/110885160-4f264680-8321-11eb-80f7-8cd6275643b8.png)
+
+```
+GH_TOKEN=[tokens]( 
+https://github.com/settings/tokens)
+GH_PAGES_USE_JSDELIVR=true
+```
+
+run the command
+
+```bash
+$ umi build
+```
+
 ## Examples
 
 * https://github.com/sorrycc/test-gh-pages
 * https://sorrycc.github.io/test-gh-pages/
+* https://github.com/alitajs/alita-docs
 
 ## LICENSE
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { IApi } from '@umijs/types';
+import { execa } from '@umijs/utils';
 import ghpages from 'gh-pages';
+import hostedGitInfo from 'hosted-git-info';
+import fetch from 'node-fetch';
+import semver from 'semver';
+import { existsSync, copyFileSync } from 'fs';
+import { join } from 'path';
 
 export default (api: IApi) => {
   const {
     paths,
     logger
   } = api;
+  const useJsdelivr = api.userConfig.ghPages?.useJsdelivr || process.env.GH_PAGES_USE_JSDELIVR === 'true';
   api.describe({
     key: 'ghPages',
     config: {
@@ -14,38 +21,103 @@ export default (api: IApi) => {
       },
     },
   });
-  function publish(args: any) {
-    const { dir, ...ghpagesArgs } = api.config.ghPages || {};
-    return new Promise((resolve, reject) => {
-      ghpages.publish(dir || paths.absOutputPath, {
-        ...ghpagesArgs,
-        ...args,
-      }, (err: any) => {
-        if (err) {
-          return reject(err);
-        }
-        // @ts-ignore
-        resolve();
-      });
+
+  if (useJsdelivr) {
+    // 自动打 tag 的情况，默认修改 publicPath
+    api.modifyDefaultConfig(async (memo) => {
+      const gitInfo = await getGitInfo();
+      return {
+        ...memo,
+        publicPath: `https://cdn.jsdelivr.net/gh/${gitInfo?.user}/${gitInfo?.project}@${gitInfo?.tag}/`,
+      };
     });
   }
 
+  const getGitInfo = async () => {
+    const { version, repository } = api.pkg;
+    if (!repository) {
+      logger.error(`Please set the repository in package.json`);
+      return;
+    }
+    // git 要先提交
+    const gitStatus = execa.sync('git', ['status', '--porcelain']).stdout;
+    if (gitStatus.length) {
+      logger.profile('publish');
+      logger.error(`Your git status is not clean. Aborting.`);
+      return;
+    }
+    // {
+    //   type: "github",
+    //   domain: "github.com",
+    //   user: "npm",
+    //   project: "hosted-git-info"
+    // }
+    const gitInfo = hostedGitInfo.fromUrl(repository?.url || repository);
+    const data = await fetch(`https://api.github.com/repos/${gitInfo?.user}/${gitInfo?.project}/tags`).then(res => res.json());
+    const versions = data.sort(function (v1: any, v2: any) {
+      return semver.compare(v2.name, v1.name);
+    });
+    // 该仓库从未设置 tag
+    const latestTag = versions[0]?.name || '0.0.1';
+    let newTag = latestTag;
+    if (semver.lt(latestTag, version)) {
+      newTag = version;
+    } else {
+      newTag = semver.inc(latestTag, 'patch')
+    }
+    return { ...gitInfo, tag: newTag };
+  }
+
+  const publish = async () => {
+    logger.profile('publish');
+    const { dir, ...ghpagesArgs } = api.config.ghPages || {};
+    // 要先执行 build
+    if (!existsSync(paths.absOutputPath as any)) {
+      logger.profile('publish');
+      logger.error(`Please execute the command 'umi build' first`);
+      return;
+    }
+    let tag = ghpagesArgs.tag;
+
+    if (useJsdelivr) {
+      const gitInfo = await getGitInfo();
+      tag = gitInfo?.tag;
+    }
+    // @ts-ignore
+    ghpages.publish(dir || paths.absOutputPath, {
+      ...ghpagesArgs,
+      tag
+    }, (err: any) => {
+      if (err) {
+        logger.profile('publish');
+        logger.error(err);
+        return;
+      }
+      logger.profile('publish');
+      logger.info('Published to Github pages');
+    });
+  }
   api.registerCommand({
     name: 'gh-pages',
     description: 'Publish to Github pages',
     details: 'umi gh-pages [Options]',
-    fn: ({ args }) => {
-      logger.profile('gh-pages');
-      publish(args).then(() => {
-        logger.profile('gh-pages');
-        logger.info('Published to Github pages');
-      }).catch(e => {
-        logger.error(e);
-      });
+    fn: async ({ args }) => {
+      publish();
+    }
+  });
+
+  api.onBuildComplete(({ err }) => {
+    //@ts-ignore
+    if (!err && !existsSync(join(paths.absOutputPath!, "404.html"))) {
+      // 如果没有 404 则复制一下 index
+      copyFileSync(
+        join(paths.absOutputPath!, "index.html"),
+        join(paths.absOutputPath!, "404.html")
+      );
+    }
+
+    if (useJsdelivr) {
+      publish();
     }
   });
 }
-// , {
-//   description: 'Publish to Github pages',
-//   usage: 'umi gh-pages [Options]',
-// }
